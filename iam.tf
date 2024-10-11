be8ca16143068e277744a690313a4dd019621615
Ericlys Moreira <ericlysm@gmail.com>
2024-10-10 22:20:27 -0300

    new: test new role

--- iam.tf
+++ iam.tf
@@ -12,6 +12,38 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
   }
 }
 
+# role para poder subir o terraform
+resource "aws_iam_role" "tf-role" {
+  name = "tf-role"
+
+  assume_role_policy = jsonencode({
+    Statement : [
+      {
+        Effect : "Allow",
+        Action : "sts:AssumeRoleWithWebIdentity",
+        Principal : {
+          Federated : "arn:aws:iam::217127483231:oidc-provider/token.actions.githubusercontent.com"
+        },
+        Condition : {
+          StringEquals : {
+            "token.actions.githubusercontent.com:aud" : [
+              "sts.amazonaws.com"
+            ],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:ericlys/eric.api.iac1:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+    Version : "2012-10-17",
+  })
+
+  tags = {
+    IAC = "True"
+  }
+}
+
 # role para poder fazer a build pelo app runner
 resource "aws_iam_role" "app-runner-role" {
   name = "app-runner-role"
